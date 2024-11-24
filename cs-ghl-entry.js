8ed901c1309a0cdbf3fe9e409ee31be84854a100
karthikraveendrancs <130793704+karthikraveendrancs@users.noreply.github.com>
2024-11-25 02:28:37 +0530

    Update cs-ghl-entry.js

--- cs-ghl-entry.js
+++ cs-ghl-entry.js
@@ -5,12 +5,23 @@ const observer = new MutationObserver((mutationsList, observer) => {
     for (const mutation of mutationsList) {
         if (mutation.type === 'childList') {
             const contactsHeader = document.querySelector('.topmenu-nav'); // Adjust selector
-            if (contactsHeader &&document.querySelector('.topmenu-navtitle').textContent.trim() === 'Contacts' && document.querySelector('.hl_wrapper.hl_contact--details') != null 
+            if (contactsHeader && document.querySelector('.topmenu-navtitle').textContent.trim() === 'Contacts' && document.querySelector('.hl_wrapper.hl_contact--details') != null 
                 && !document.querySelector('.custom-button')) {
                 const customButton = document.createElement('button');
                 customButton.innerText = 'Create Patient';
                 customButton.className = 'custom-button';
                 customButton.style = 'margin-left: 10px; background-color: #42ba78; color: white; padding: 5px; border: none; cursor: pointer;';
+                customButton.onclick = function() {
+                    alert('Custom button clicked!');
+                };
+                contactsHeader.appendChild(customButton);
+            }
+            if (document.querySelector('#CreateUpdateOpportunity') && !document.querySelector('#button-create-patient-opportunity')){
+                const customButton = document.createElement('button');
+                customButton.innerText = 'Create Patient';
+                customButton.id = 'button-create-patient-opportunity';
+                customButton.className = 'n-button n-button--primary-type n-button--medium-type h-11 min-w-[8rem]'
+                customButton.style = 'background-color: #42ba78; color: white;';
                 customButton.onclick = function() {
                     alert('Custom button clicked!');
                 };
