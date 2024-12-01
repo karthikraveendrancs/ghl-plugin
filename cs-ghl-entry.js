fe3b68cfec437bb8e0f4a44dee72cb6ceb64b496
karthikraveendrancs <130793704+karthikraveendrancs@users.noreply.github.com>
2024-12-02 01:08:29 +0530

    Update cs-ghl-entry.js

--- cs-ghl-entry.js
+++ cs-ghl-entry.js
@@ -6,8 +6,7 @@ const observer = new MutationObserver((mutationsList, observer) => {
     for (const mutation of mutationsList) {
         if (mutation.type === 'childList') {
             const contactsHeader = document.querySelector('.topmenu-nav'); // Adjust selector
-            if (contactsHeader && document.querySelector('.topmenu-navtitle').textContent.trim() === 'Contacts' && document.querySelector('.hl_wrapper.hl_contact--details') != null 
-                && !document.querySelector('.custom-button')) {
+            if (contactsHeader && document.querySelector('.topmenu-navtitle').textContent.trim() === 'Contacts' ) {
                 const csIdentifier = document.querySelector('#contact\\.carestack_identifier');
                 if(!csIdentifier){
                     return;
