8d3f053cb23414482c1fdc68050f4d6ae8435e5d
karthikraveendrancs <130793704+karthikraveendrancs@users.noreply.github.com>
2024-12-03 02:38:29 +0530

    Update cs-ghl-entry.js

--- cs-ghl-entry.js
+++ cs-ghl-entry.js
@@ -83,9 +83,9 @@ const createPatient = function() {
 }
 
 const loadLocations = async function(tenant) {
+    const token = await getToken();
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
-        const token = await getToken();
         xhr.open("GET", baseUrl + `tenants/${tenant}/locations`, true);
         xhr.setRequestHeader('Authorization', token);
         xhr.onload = function () {
